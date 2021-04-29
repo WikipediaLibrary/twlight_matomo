9a492f6918c7ba68f4f8c50cac76aef3d4e64dcf
jsn <jsherman@wikimedia.org>
2021-04-29 11:32:56 -0500

    backup filenames include date and time

--- bin/backup.sh
+++ bin/backup.sh
@@ -2,7 +2,7 @@
 
 source /secrets.sh
 
-dow=$( date +%a | awk '{print tolower($0)}')
+dt=$( date +'%Y-%m-%d-%H-%M' )
 
 echo 'Backup Started'
 ## Perform db dump
@@ -10,7 +10,7 @@ mysqldump -h ${MATOMO_DATABASE_HOST} -u ${MATOMO_DATABASE_USERNAME} -p"${MATOMO_
 # Root only
 chmod 0600 /tmp/${MATOMO_DATABASE_DBNAME}.sql
 # Create tarball
-tar -czf /srv/matomo/backup/${MATOMO_FQDN}.${dow}.tar.gz -C / /var/www/html/config/config.ini.php /var/www/html/plugins /tmp/${MATOMO_DATABASE_DBNAME}.sql
+tar -czf /srv/matomo/backup/${MATOMO_FQDN}.${dt}.tar.gz -C / /var/www/html/config/config.ini.php /var/www/html/plugins /tmp/${MATOMO_DATABASE_DBNAME}.sql
 # Cleanup db dump
 rm /tmp/${MATOMO_DATABASE_DBNAME}.sql
 echo 'Backup Complete'
